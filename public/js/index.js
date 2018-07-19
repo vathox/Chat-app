a217e7dab9d8445804c4d093b54e2ca3b057f18a
vathox <dzierzko.a@gmail.com>
2018-07-18 23:45:54 -0400

    Scroll messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 const socket = io();
 
+
+function scrollPage () {
+    // Selectors
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child')
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', () => {
     console.log('Connected to server');
 });
@@ -18,8 +35,9 @@ socket.on('newMessage', (message) => {
         createdAt: formattedTime
     });
 
-    $('#messages').append(html)
+    $('#messages').append(html);
 
+    scrollPage()
 });
 
 socket.on('newLocationMessage', (message) =>{
@@ -32,9 +50,9 @@ socket.on('newLocationMessage', (message) =>{
         createdAt: formattedTime
     });
 
-    $('#messages').append(html)
+    $('#messages').append(html);
 
-
+    scrollPage()
 
 });
 
